0d1c08292c9d81b88487b407838ac35e2dc1eab9
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-24 17:50:20 -0600

    style: Add Card component to page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Card from "@/components/Card";
 import Header from "@/components/Header";
 
 
 const page = () => {
   return (
+    <>
     <div className="">
       <Header />
     </div>
+      <Card />
+      </>
   )
 };
 
