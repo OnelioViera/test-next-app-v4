d0f8500f98802e232a43bb919857a59dcf5fe3a2
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-23 22:33:00 -0600

    style: Update link URL in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
           <h1 className="text-3xl lg:text-5xl md:text-white md:text-4xl text-white font-bold text-center">Main Page</h1>
             <p className="text-center text-white mt-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nam officiis praesentium architecto odio quisquam a illum, minus reiciendis, aperiam est consectetur ratione vitae doloribus vel aut hic voluptas dolorem!</p>
             <button className="mt-10">
-            <Link href="/link" className="bg-red-500 text-white py-3 px-6 mt-6 rounded-lg">Link 1</Link>
+            <Link href="/link_1" className="bg-red-500 text-white py-3 px-6 mt-6 rounded-lg">Link 1</Link>
           </button>
           </div>
 
