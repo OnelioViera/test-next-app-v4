4c1be2dfcef7fa5cd953433c171bd3d2b19eaa04
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-25 10:56:31 -0600

    style: Update Card component import in page.tsx

--- components/MainCard.tsx
+++ components/MainCard.tsx
@@ -1,13 +1,17 @@
-const Card = () => {
+import Link from "next/link";
+
+const Card = ({ MainCard }) => {
   return (
     <>
       <div className="bg-gray-100 max-w-96 rounded-t-lg shadow-lg">
-        <h1 className="text-3xl text-gray-600 font-semibold text-center py-6">This is a Card.</h1>
+        <h1 className="text-3xl text-gray-600 font-semibold text-center py-6">Title</h1>
       <div className="bg-white py-6 px-4 rounded-b-lg text-center">
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ea numquam veritatis nihil necessitatibus unde, dolor velit, voluptas, amet et esse? Sint qui necessitatibus modi repellat blanditiis voluptatibus aperiam odit.</p>
-          <button className="mt-6">
-          <a href="#" className="bg-gray-500 hover:bg-gray-600 hover:font-bold shadow-lg text-white py-2 px-6 rounded-lg">Link 1</a>
-        </button>
+          <div className="mt-6">
+          <Link href="#" className="bg-gray-500 hover:bg-gray-600 hover:font-bold shadow-lg text-white py-2 px-6 rounded-lg">
+          Button
+            </Link>
+            </div>
         </div>
 
       </div>
