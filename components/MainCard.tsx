71c02624679a298d93b7f25ee778121532920c11
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-25 11:16:53 -0600

    style: Update MainCard component layout and button styling

--- components/MainCard.tsx
+++ components/MainCard.tsx
@@ -7,7 +7,7 @@ const MainCard = () => {
         <h1 className="text-3xl text-gray-600 font-semibold text-center py-6">Title</h1>
       <div className="bg-white py-6 px-4 rounded-b-lg text-center">
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ea numquam veritatis nihil necessitatibus unde, dolor velit, voluptas, amet et esse? Sint qui necessitatibus modi repellat blanditiis voluptatibus aperiam odit.</p>
-          <div className="mt-6">
+          <div className="mt-8 mb-4">
           <Link href="#" className="bg-gray-500 hover:bg-gray-600 hover:font-bold shadow-lg text-white py-2 px-6 rounded-lg">
           Button
             </Link>
